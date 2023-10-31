3510e07904ebacf5afa13edfe833273f1064cd3d
JoseHenriquePatrocinio <henriquepatrocinio98@gmail.com>
2023-10-30 21:44:08 -0300

    Initial commit

---
+++ APIProdutos/Data/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace APIProdutos.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Produto>()
+                .Property(p => p.Nome)
+                    .HasMaxLength(80);
+
+            modelBuilder.Entity<Produto>()
+                .Property(p => p.Preco)
+                    .HasPrecision(10, 2);
+
+            modelBuilder.Entity<Produto>()
+                .HasData(
+                    new Produto { Id = 1, Nome = "Caderno", Preco = 7.95M, Estoque = 10 },
+                    new Produto { Id = 2, Nome = "Borracha", Preco = 2.95M, Estoque = 30 },
+                    new Produto { Id = 3, Nome = "Estojo", Preco = 6.95M, Estoque = 15 });
+        }
+    }
+}
---
+++ APIProdutos/Migrations/20231031001308_Inicial.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace APIProdutos.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(type: "varchar(80)", maxLength: 80, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Preco = table.Column<decimal>(type: "decimal(10,2)", precision: 10, scale: 2, nullable: false),
+                    Estoque = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.InsertData(
+                table: "Produtos",
+                columns: new[] { "Id", "Estoque", "Nome", "Preco" },
+                values: new object[,]
+                {
+                    { 1, 10, "Caderno", 7.95m },
+                    { 2, 30, "Borracha", 2.95m },
+                    { 3, 15, "Estojo", 6.95m }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produtos");
+        }
+    }
+}
